9163a8d9beab66366b29544c800e7e3ecc29a90a
Twin <jarrettsneed2@gmail.com>
2026-07-17 01:38:13 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,19 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            eprintln!("Logs from your program will appear here!");
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                eprintln!("Failed to read file {}", filename);
+                eprintln!("Failed to read file {filename}");
                 String::new()
             });
 
-            // TODO: Uncomment the code below to pass the first stage
-            // if !file_contents.is_empty() {
-            //     panic!("Scanner not implemented");
-            // } else {
-            //     println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
-            // }
+            if file_contents.is_empty() {
+                println!("EOF  null");
+            } else {
+                panic!("Scanner not implemented");
+            }
         }
         _ => {
-            eprintln!("Unknown command: {}", command);
+            eprintln!("Unknown command: {command}");
         }
     }
 }
